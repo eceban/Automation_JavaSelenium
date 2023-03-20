d916b4a3cd2a5f5521086fe82ca1be7490824c68
Evghenia CEBAN <eceban@pentalog.com>
2023-03-21 00:08:42 +0200

    chromedriver fix

--- src/test/java/testNGAnnotations/BeforeAfter.java
+++ src/test/java/testNGAnnotations/BeforeAfter.java
@@ -1,5 +1,6 @@
 package testNGAnnotations;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -19,10 +20,17 @@ public class BeforeAfter {
         }
 */
         // setam driverul pentru browser chrome
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--window-size-1920,1080");
+        chromeOptions.addArguments("--no-sandbox");
+        WebDriverManager.chromedriver().setup();
 
         // facem o instanta de chrome
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(chromeOptions);
 
         // accesam un anumit URL
         driver.get("https://demo.automationtesting.in/Index.html");
